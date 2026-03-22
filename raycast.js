ec3bacb288e3285e52f398d91e4d11caded6339f
fabioalmeida08 <fabioalmeida0896@gmail.com>
2026-03-22 15:13:36 -0300

    add type ref to p5

--- raycast.js
+++ raycast.js
@@ -1,4 +1,5 @@
-const TILE_SIZE = 64;
+/// <reference path="./global.d.ts" />
+const TILE_SIZE = 32;
 const MAP_NUM_ROWS = 11;
 const MAP_NUM_COLS = 15;
 const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
@@ -6,20 +7,52 @@ const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 
 class Map {
   constructor() {
-      this.grid = [
-          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-          [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-          [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
-          [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-          [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-      ];
+    this.grid = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+      [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+      [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+  }
+
+  render() {
+    for (var i = 0; i < MAP_NUM_ROWS; i++)
+    {
+      for (var j = 0; j < MAP_NUM_COLS; j++)
+      {
+        var tileX = j * TILE_SIZE;
+        var tileY = i * TILE_SIZE;
+        var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff"
+        stroke("#222")
+        fill(tileColor)
+        rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+      }
     }
+  }
+}
+
+var grid = new Map();
+
+function setup() {
+  //inicilizar os objetos
+  createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 
 }
 
+function draw() {
+  //renderizar todos objetos
+  update();
+  grid.render();
+}
+
+function update() {
+  //rendereizar objetos frame por frame
+
+}
